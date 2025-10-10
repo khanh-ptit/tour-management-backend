f15a0c424118bb5614242ff43d452e9a43bb16fc
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2025-10-11 00:02:35 +0700

    feature/role permission tours api

--- api/v1/middlewares/admin/auth.middleware.js
+++ api/v1/middlewares/admin/auth.middleware.js
@@ -1,8 +1,9 @@
 const jwt = require("jsonwebtoken");
+const Role = require("../../models/role.model");
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-module.exports.requireAuth = (req, res, next) => {
+module.exports.requireAuth = async (req, res, next) => {
   const token = req.cookies.token;
   if (!token) {
     return res.status(401).json({ message: "Bạn chưa đăng nhập" });
@@ -11,6 +12,8 @@ module.exports.requireAuth = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
     req.user = decoded;
+    const role = await Role.findOne({ _id: decoded.roleId });
+    req.role = role;
     next();
   } catch (error) {
     return res.status(401).json({ message: "Token không hợp lệ" });

19feb7e880539d660a6f6dd786eb9c8e118e0462
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2025-11-02 18:31:16 +0700

    feature/captcha integration

--- api/v1/middlewares/client/auth.middleware.js
+++ api/v1/middlewares/client/auth.middleware.js
@@ -4,7 +4,8 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 module.exports.requireAuth = async (req, res, next) => {
   const authHeader = req.headers["authorization"];
-  const token = authHeader.split(" ")[1];
+  const token = authHeader?.split(" ")[1] || "";
+
   if (!token) {
     return res.status(401).json({ message: "Bạn chưa đăng nhập" });
   }
